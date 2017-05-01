87911cdb1be3c3b592614b25a86ff36023811c95
Starlynne <lewis.start0905@gmail.com>
2017-05-01 11:39:06 -0700

    Add functionality, views and integration_specs to build ui portion of app.

--- app.rb
+++ app.rb
@@ -1,19 +1,39 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
+require('./lib/list')
 require("./lib/task")
 require('pg')
 DB = PG.connect({:dbname => 'to_do'})
 
 get('/') do
-  @tasks = Task.all()
   erb(:index)
 end
 
-post('/tasks_form') do
-  description = params.fetch('description')
-  task1 = Task.new(description)
-  task1.save()
-  @tasks = Task.all()
-  erb(:index)
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+ end
+
+ get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i()
+  @list = List.find(list_id)
+  @task = Task.new({:description => description, :list_id => list_id})
+  @task.save()
+  erb(:task_success)
 end
